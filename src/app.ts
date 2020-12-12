33d3f607e970c6bfcacaba220e130f91ad7dbe61
Jan Willem Harmelink <janwillem@harmelink.net>
2020-12-12 21:15:20 +0100

    .env

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import cors from 'cors';
+import dotenv from 'dotenv';
 import express, {Request, Response} from 'express';
 import mongoose from "mongoose";
 import {graphqlHTTP} from 'express-graphql';
 import {buildSchema} from 'graphql';
 import xkcdService from "./services/xkcd-service";
 import imageService from "./services/image-service";
+
+dotenv.config();
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
@@ -30,7 +33,7 @@ const root = {
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:3000', 'xkcd.harmelink.net']
+    origin: [process.env.ORIGIN]
 }))
 app.use('/api/graphql', graphqlHTTP({
     schema: schema,
@@ -42,7 +45,7 @@ app.get('/api/images/:filename', (request: Request, response: Response) => image
 app.listen(4000);
 
 // Set up default mongoose connection
-mongoose.connect('mongodb://localhost:27017/xkcd', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/xkcd`, {useNewUrlParser: true, useUnifiedTopology: true});
 // Bind connection to error event (to get notification of connection errors)
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
